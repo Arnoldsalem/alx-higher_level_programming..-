91905f16cc30d1c7f221efd80334bdf096a4337f
Arnoldsalem <arnoldsalem@gmail.com>
2023-11-30 07:16:42 -0800

    reworked

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def add(a, b):
     return a + b
-
 # main program
 a = 1
 b = 2
-
 from add_0 import add
-
 result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+iprint("{} + {} = {}".format(a, b, result))
