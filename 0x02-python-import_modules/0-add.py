3fd0fdfe99dbfbf643d93f8a595b95bdf9f8c7b3
Arnoldsalem <arnoldsalem@gmail.com>
2023-11-30 04:38:29 -0800

    addition wgile importing

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
-from add_0 import add
+def add(a, b):
+    return a + b
+
+# main program
 a = 1
 b = 2
-print("{0} + {1} = {2}".format(a, b, add(a, b)))
+
+from add_0 import add
+
+result = add(a, b)
+print("{} + {} = {}".format(a, b, result))
